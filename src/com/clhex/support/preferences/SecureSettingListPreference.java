529e2c82bc651fdf579d9d872c259def8e7ef501
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-02-21 21:07:17 -0300

    Rework preferences
    Don't crash if default is not provided
    
    Change-Id: Ic745c0e12790a1ff856df927170ed9a8e2d4583f
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- src/com/clhex/support/preferences/SecureSettingListPreference.java
+++ src/com/clhex/support/preferences/SecureSettingListPreference.java
@@ -19,31 +19,31 @@ package com.clhex.support.preferences;
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-
+ 
 public class SecureSettingListPreference extends ListPreference {
-
-    public SecureSettingListPreference(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
-    }
-
-    public SecureSettingListPreference(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
-    }
-
-    public SecureSettingListPreference(Context context) {
-        super(context);
-        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
-    }
-
-    @Override
-    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-        // This is what default ListPreference implementation is doing without respecting
-        // real default value:
-        //setValue(restoreValue ? getPersistedString(mValue) : (String) defaultValue);
-        // Instead, we better do
-        setValue(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue);
-    }
-
-}
+ 
+     public SecureSettingListPreference(Context context, AttributeSet attrs, int defStyle) {
+         super(context, attrs, defStyle);
+         setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+     }
+ 
+     public SecureSettingListPreference(Context context, AttributeSet attrs) {
+         super(context, attrs);
+         setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+     }
+ 
+     public SecureSettingListPreference(Context context) {
+         super(context);
+         setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+     }
+ 
+     @Override
+     protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+         // This is what default ListPreference implementation is doing without respecting
+         // real default value:
+         //setValue(restoreValue ? getPersistedString(mValue) : (String) defaultValue);
+         // Instead, we better do
+         setValue(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue);
+     }
+ 
+ }
