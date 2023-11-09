8df7edf9f248132a1e70fedffa352d15f0e86060
clhex <clhexftw@protonmail.com>
2023-11-09 22:27:17 +0530

    fixup! 71cf32e02d1

--- src/com/android/settings/custom/BatterySettingExtras.java
+++ src/com/android/settings/custom/BatterySettingExtras.java
@@ -1,7 +1,5 @@
 package com.android.settings.custom;
 
-import static com.android.settings.fuelgauge.BatteryBroadcastReceiver.BatteryUpdateType;
-
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.database.ContentObserver;
@@ -18,14 +16,6 @@ import androidx.preference.Preference;
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.Utils;
-import com.android.settings.fuelgauge.BatteryHeaderPreferenceController;
-import com.android.settings.fuelgauge.BatteryInfo;
-import com.android.settings.fuelgauge.BatteryInfoLoader;
-import com.android.settings.fuelgauge.BatteryUtils;
-import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
-import com.android.settings.fuelgauge.batterytip.BatteryTipLoader;
-import com.android.settings.fuelgauge.batterytip.BatteryTipPreferenceController;
-import com.android.settings.fuelgauge.batterytip.tips.BatteryTip;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
@@ -39,38 +29,6 @@ import java.util.List;
 public class BatterySettingExtras extends SettingsPreferenceFragment {
 
     private static final String TAG = "BatterySettingExtras";
-    private static final String KEY_BATTERY_TEMP = "battery_temp";
-
-    @VisibleForTesting
-    Preference mBatteryTempPref;
-
-    @VisibleForTesting
-    void restartBatteryInfoLoader() {
-        if (getContext() == null) {
-            return;
-        }
-        // Skip restartBatteryInfoLoader if battery is not present.
-        if (!mIsBatteryPresent) {
-            return;
-        }
-        restartLoader(LoaderIndex.BATTERY_INFO_LOADER, Bundle.EMPTY, mBatteryInfoLoaderCallbacks);
-    }
-
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        mBatteryTempPref = (Preference) findPreference(KEY_BATTERY_TEMP);
-    }
-
-    protected void refreshUi(@BatteryUpdateType int refreshType) {
-        final Context context = getContext();
-        if (BatteryInfo.batteryTemp != 0f) {
-            mBatteryTempPref.setSummary(BatteryInfo.batteryTemp / 10 + " °C");
-        } else {
-            mBatteryTempPref.setSummary(getResources().getString(R.string.status_unavailable));
-        }
-        restartBatteryInfoLoader();
-    }
 
     @Override
     public int getMetricsCategory() {
